9a8e9054ef15d21bffa32ce49a7544d0da049743
Цомкалов Игорь <igorcomkalov@gmail.com>
2024-05-06 02:24:16 +0300

    Добавьте файлы проекта.

---
+++ Laba11(W)/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    // Объявление делегата
+    delegate string DayOfWeekDelegate();
+
+    static void Main(string[] args)
+    {
+        // Массив с названиями дней недели
+        string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+        // Индекс текущего дня недели (начинается с понедельника)
+        int currentDayIndex = 0;
+
+        // Создание экземпляра делегата с помощью лямбда-выражения
+        DayOfWeekDelegate getDayOfWeek = () =>
+        {
+            string day = daysOfWeek[currentDayIndex];
+            currentDayIndex = (currentDayIndex + 1) % daysOfWeek.Length; // Переход к следующему дню, циклически
+            return day;
+        };
+
+        // Вызов делегата несколько раз
+        for (int i = 0; i < 15; i++)
+        {
+            Console.WriteLine(getDayOfWeek());
+        }
+
+        Console.ReadLine();
+    }
+}
